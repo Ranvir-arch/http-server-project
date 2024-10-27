96d8605d6a52b6591818d9bd71e7a6fdff280eae
ranvir <yadavranvir943@gmail.com>
2024-10-27 05:39:01 +0530

    concurrent connections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,10 +17,54 @@ class ClientHandler implements Runnable {
   public void run() {
     try {
       BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      String requeString = in.readLine();
-      if (requeString != null) {
-        String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-        clientSocket.getOutputStream().write(httpResponse.getBytes());
+      String requestLine = in.readLine();
+      if (requestLine != null && !requestLine.isEmpty()) {
+        System.out.println(requestLine);
+        Map<String, String> headers = new HashMap<>();
+        String headerLine;
+        while ((headerLine = in.readLine()) != null && !headerLine.isEmpty()) {
+          String[] headerParts = headerLine.split(": ", 2);
+          if (headerParts.length == 2) {
+            headers.put(headerParts[0], headerParts[1]);
+          }
+        }
+        String message = "";
+        if (headers.containsKey("User-Agent")) {
+          message = headers.get("User-Agent");
+
+          // System.out.println(httpResponse);
+
+        } else {
+          String[] requestParts = requestLine.split(" ");
+
+          if (requestParts[1].startsWith("/echo/")) {
+            message = requestParts[1].substring("/echo/".length());
+          } else if (requestParts[1].equals("/")) {
+            message = "000";
+          } else {
+            message = "XXX";
+          }
+        }
+        if (message.equals("XXX")) {
+          clientSocket.getOutputStream().write(
+              "HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        } else if (message.equals("000")) {
+          clientSocket.getOutputStream().write(
+              "HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            } else {
+              String contentType = "text/plain"; // Specify the type of content
+              int contentLength = message.length(); // Get the length of the response body
+              System.out.println(message);
+              // Build HTTP response with headers
+              String httpResponse = "HTTP/1.1 200 OK\r\n" +
+                  "Content-Type: " + contentType + "\r\n" +
+                  "Content-Length: " + contentLength + "\r\n" +
+                  "\r\n" +
+                  message;
+              clientSocket.getOutputStream().write(
+                  httpResponse.getBytes());
+    
+            }    
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
